3b63cd4dbcfdd6e5bffd0c5fd8291477c3864cff
Artem Chubenko <artem.chubenko@gmail.com>
2022-07-15 22:57:22 +0300

    Update contactForm.jsx

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -30,7 +30,7 @@ export default class Form extends Component {
           type="text"
           name="name"
           value={this.state.name}
-          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={this.onChange}
@@ -40,7 +40,7 @@ export default class Form extends Component {
           type="tel"
           name="number"
           value={this.state.number}
-          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={this.onChange}
