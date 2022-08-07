78d901ce44bf61375e06690c0fe10b94b4ce42e1
Artem Chubenko <artem.chubenko@gmail.com>
2022-08-07 21:40:20 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import ContactForm from './contactForm';
 import Filter from './filter';
 import MarkUp from './markUp';
